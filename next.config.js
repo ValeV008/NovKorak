fca635b5198577df19058f212525cbbb9f12acf6
Valentin <kragelj.valentin@yahoo.com>
2025-03-23 23:44:52 +0100

    Enable to use next/images on Netlify.

--- next.config.js
+++ next.config.js
@@ -1,14 +1,22 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
 });
 
 module.exports = withBundleAnalyzer({
   poweredByHeader: false,
   trailingSlash: true,
-  basePath: '',
+  basePath: "",
   // The starter code load resources from `public` folder with `router.basePath` in React components.
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
   reactStrictMode: true,
+  output: "standalone",
+  images: {
+    // Enable optimization for images from the public directory
+    loader: "default",
+    domains: [], // Don't need to specify external domains since you use local images
+    path: "/_next/image", // This ensures Next.js uses the right path for image optimization
+    unoptimized: false, // Default to enable optimization
+  },
 });
