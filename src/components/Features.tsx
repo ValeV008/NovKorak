0d37bed4dae21fb872c3c2046a44c1f454501954
Valentin Kragelj <valentin.kragelj@endava.net>
2025-08-01 16:11:42 +0200

    Smaller UI fix.

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -10,7 +10,7 @@ const Features = () => {
   return (
     <div className={`py-12 bg-background`} id="features">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="lg:text-center">
+        <div className="lg:text-center pb-10">
           <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
             {subtitle}
           </p>
