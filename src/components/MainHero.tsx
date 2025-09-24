fd0c3aef10039ff2f7f9353707818bd4f1ed1f3f
Valentin Kragelj <valentin.kragelj@endava.net>
2025-09-24 12:23:17 +0200

    Added "na domu" upper text on homepage.

--- src/components/MainHero.tsx
+++ src/components/MainHero.tsx
@@ -6,7 +6,7 @@ const MainHero = () => {
   const { mainHero } = config;
 
   return (
-    <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-15">
+    <main className="mt-10 ml-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-15">
       <div className="sm:text-center lg:text-left">
         <div className="mb-2 flex justify-center lg:justify-center">
           <button
@@ -16,13 +16,22 @@ const MainHero = () => {
             Kliknite za 10 min brezplačni posvet
           </button>
         </div>
-        <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-          <span className="block xl:inline">{mainHero.title}</span>{" "}
+        <h1 className="relative text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl xl:mt-10">
+          <span className="relative inline-block">
+            {mainHero.title}
+            <span className="absolute lg:-top-1 lg:-right-13 text-xl text-gray-500 font-semibold whitespace-nowrap">
+              {mainHero.titleUpper}
+            </span>
+          </span>
+          <br />
           <span className={`block text-primary xl:inline`}>
             {mainHero.subtitle}
           </span>
         </h1>
         <br />
+        <p className="mt-3 text-base text-gray-700 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+          {mainHero.additionalText}
+        </p>
         <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
           Kaj delovna terapija lahko ponudi vam?
         </p>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,9 +18,9 @@ const App = () => {
   return (
     <div className={`bg-background grid gap-y-16 overflow-hidden`}>
       <div className={`relative bg-background`}>
-        <div className="max-w-7xl mx-auto">
+        <div className="max-w-7xl ml-[10%]">
           <div
-            className={`relative z-10 pb-8 bg-background sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32`}
+            className={`relative z-10 pb-8 bg-background sm:pb-16 md:pb-20 lg:max-w-3xl lg:w-full lg:pb-28 xl:pb-32`}
           >
             <Header />
             <MainHero />
