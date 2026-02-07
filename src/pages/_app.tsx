a246048dad6b7604d50833d355f7c5106f23e91f
Valentin Kragelj <valentin.kragelj@endava.net>
2026-02-08 00:36:13 +0100

    Added toploader.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { appWithTranslation } from "next-i18next";
+import NextTopLoader from "nextjs-toploader";
 
 import { AppProps } from "next/app";
 import Head from "next/head";
@@ -10,6 +11,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => (
     <Head>
       <title>Nov Korak</title>
     </Head>
+    <NextTopLoader color="#f2b70d" showSpinner={false} />
     <Component {...pageProps} />
   </>
 );
